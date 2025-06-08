041c3209c947c63a1fe6ac5ee6d32bf7987aac7a
ShadowOsmium <gemini35722@gmail.com>
2025-06-09 02:29:43 +1000

    Fixed UI Scaling With Wide Screen

--- Assets/Scripts/Assembly-CSharp/GameUIAutoSize.cs
+++ Assets/Scripts/Assembly-CSharp/GameUIAutoSize.cs
@@ -2,19 +2,16 @@ using UnityEngine;
 
 public class GameUIAutoSize : MonoBehaviour
 {
-	private void Start()
-	{
-		float num = Screen.width;
-		float num2 = Screen.height;
-		if (num == 2048f || num2 == 2048f)
-		{
-			num /= 2f;
-			num2 /= 2f;
-		}
-		num = 1136f;
-		num2 = 640f;
-		float num3 = num / 960f;
-		float num4 = num2 / 640f;
-		base.transform.localScale = new Vector3(base.transform.localScale.x * num3, base.transform.localScale.y * num4, base.transform.localScale.z);
-	}
+    public Vector2 referenceResolution = new Vector2(960f, 640f);
+
+    private void Start()
+    {
+        float screenWidth = Screen.width;
+        float scaleX = screenWidth / referenceResolution.x;
+
+        transform.localScale = new Vector3(
+            transform.localScale.x * scaleX,
+            transform.localScale.y * scaleX,
+            transform.localScale.z);
+    }
 }
